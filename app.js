ad9edf6e07556bfd47bdfce8b852e73e68d98c29
Rokosz <legogattrooper@gmail.com>
2019-07-12 19:56:57 -0400

    updated from json config to javscript.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Client();
 
-const config = require(__dirname + '/config/config.json') // using __dirname + 'dirpath' to avoid directory issues
+const config = require(__dirname + '/config/config.js') // using __dirname + 'dirpath' to avoid directory issues
 
 require(__dirname + '/src/handlers/command.js')(client);
 
