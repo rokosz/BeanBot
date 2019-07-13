614a20ed95168abc7f6fa5fdd2a918ab768aa88f
Rokosz <legogattrooper@gmail.com>
2019-07-12 20:15:56 -0400

    added two events

--- src/util/eventLoader.js
+++ src/util/eventLoader.js
@@ -5,5 +5,7 @@ module.exports = (client) => {
   // TODO: set up logging funcitons
 
   client.on('ready', () => beanLoad('ready')(client));
+  client.on('guildMemberAdd', beanLoad('GMA'));
+  client.on('guildMemberRemove', beanLoad('GMR'));
 
 }
