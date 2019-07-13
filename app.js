d92c3d6672884cfdd2a900d685e9a77e2b31aa3c
Rokosz <legogattrooper@gmail.com>
2019-07-12 21:19:26 -0400

    updated Client/included command handler.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const Discord = require('discord.js');
-const client = new Client();
+const client = new Client({
+  disableEveryone: true,
+  autoReconnect: true,
+  forceFetchUsers: true
+});
 
 const config = require(__dirname + '/config/config.js') // using __dirname + 'dirpath' to avoid directory issues
 
-require(__dirname + '/src/handlers/command.js')(client);
+require(__dirname + '/src/handlers/commandHandler.js')(client);
 
 client.login(config.token);
