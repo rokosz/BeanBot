b62e4b9bbdb866b3e469e6cd49fc6a44c46ca904
Rokosz <legogattrooper@gmail.com>
2019-07-12 21:20:42 -0400

    updated names

--- src/util/eventLoader.js
+++ src/util/eventLoader.js
@@ -1,11 +1,11 @@
-const beanLoad = (event) => require(__dirname + `/../events/${event}`);
+const bean = (event) => require(__dirname + `/../events/${event}`);
 
 module.exports = (client) => {
 
   // TODO: set up logging funcitons
 
-  client.on('ready', () => beanLoad('ready')(client));
-  client.on('guildMemberAdd', beanLoad('GMA'));
-  client.on('guildMemberRemove', beanLoad('GMR'));
+  client.on('ready', () => bean('ready')(client));
+  client.on('guildMemberAdd', bean('GMA'));
+  client.on('guildMemberRemove', bean('GMR'));
 
 }
