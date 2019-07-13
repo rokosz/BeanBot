fba0b08038652edf961717c7b98636876282737e
Rokosz <legogattrooper@gmail.com>
2019-07-12 20:16:19 -0400

    setup logging and all the other jazz

---
+++ src/events/GMA.js
@@ -0,0 +1,11 @@
+const { RichEmbed } = require('discord.js');
+const chalk = require('chalk'); // CLI coloring
+const { logChannel, db } = require(__dirname + '/../config/config.js');
+
+// TODO: setup user database for people joining.
+
+module.exports = async member => {
+
+
+
+}
