d2fd12a38bd66c93c115cf6e911a68857d1633ec
Rokosz <legogattrooper@gmail.com>
2019-07-14 01:09:09 -0400

    set up base parts

--- src/commands/warn.js
+++ src/commands/warn.js
@@ -2,6 +2,15 @@ module.exports.run = async (client, message, args) => {
 
   if(message.member.roles.some(r => ['', ''].includes(r.name))) {
 
+    let bean = message.members.first();
+
+    if(!member)
+      return message.reply("No user supplied.");
+    if(!member.bannable)
+      return message.reply("Can't bean this person.");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason needed."
 
 
   }
