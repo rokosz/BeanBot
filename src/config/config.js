7a9e4aedfbebc3bde0f1f770f194e82e6a032bb2
Rokosz <legogattrooper@gmail.com>
2019-07-13 21:23:13 -0400

    included timestamp/dependancy

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 const config = {
   "token": "you thought.",
   "googleAPI": "TBD on use",
@@ -6,3 +8,4 @@ const config = {
 exports.db = 'yeet';
 exports.prefixArray = ['b.', 'b!', '-b'];
 exports.supportArray = ['userid array here'];
+exports.timestamp = `${moment().format("YYYY-MM-DD HH:mm:ss")}`;
