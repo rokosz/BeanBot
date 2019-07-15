37a28787e22daebdae8623464e5337bf378338e5
rokosz <46833498+rokosz@users.noreply.github.com>
2019-07-15 13:36:07 -0400

    updated to match similar with the ban command

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -12,9 +12,19 @@ module.exports.run = async (client, message, args) => {
     let reason = args.slice(1).join(' ');
     if(!reason) reason = "No reason needed."
 
+    const kE = new RichEmbed();
+      .setTitle('User kicked')
+      .setAuthor(member.user.username)
+      .addField('reason', reason)
+      .setFooter(timestamp)
+
+
     member.kick().then((member) => {
 
       // TODO: setup logging and database blacklisting.
+
+      message.channel.send(kE);
+
       sql.prepare("INSET INTO kicks VALUES (?)").then(b => b.run([message.author.id]));
       console.log(chalk.yellow(`[${member.guild}]`) + `${member.user.username} was kicked`);
 
@@ -22,6 +32,7 @@ module.exports.run = async (client, message, args) => {
 
       // TODO: same as above for failures (mod attempting mod/admin ban)
 
+      message.channel.send('user unbannable');
       console.log(chalk.yellow(`[${member.guild}]`) + `${member.user.username} is unkickable`);
 
     });
