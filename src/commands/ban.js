eafd40b61d5bb3a648d1689d202f77cf32c0c2ad
rokosz <46833498+rokosz@users.noreply.github.com>
2019-07-15 13:29:32 -0400

    database usage prompted.

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -1,3 +1,9 @@
+const { RichEmbed } = require('discord.js');
+const chalk = require('chalk');
+
+const sql = require('sqlite');
+await sql.open(__dirname + '/../databases/db.sqlite');
+
 module.exports.run = async (client, message, args) => {
 
   if(message.member.roles.some(r => ['', ''].includes(r.name))) {
@@ -13,11 +19,17 @@ module.exports.run = async (client, message, args) => {
     if(!reason) reason = "No reason needed."
 
     member.ban().then((member) => {
+
       // TODO: setup logging and database blacklisting.
+      sql.prepare("INSET INTO bans VALUES (?)").then(b => b.run([message.author.id]));
+      console.log(chalk.yellow(`[${member.guild}]`) + `${member.user.username} was banned`);
 
     }).catch(() => {
+
       // TODO: same as above for failures (mod attempting mod/admin ban)
-      
+
+      console.log(chalk.yellow(`[${member.guild}]`) + `${member.user.username} is unbannable`);
+
     })
 
   }
