2e67c80a2e691c5fd5a372e37cea1e1fd1bc5661
rokosz <46833498+rokosz@users.noreply.github.com>
2019-07-15 13:29:22 -0400

    updated kick command to allow for usage with the database

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -13,12 +13,18 @@ module.exports.run = async (client, message, args) => {
     if(!reason) reason = "No reason needed."
 
     member.kick().then((member) => {
+
       // TODO: setup logging and database blacklisting.
+      sql.prepare("INSET INTO kicks VALUES (?)").then(b => b.run([message.author.id]));
+      console.log(chalk.yellow(`[${member.guild}]`) + `${member.user.username} was kicked`);
 
     }).catch(() => {
+
       // TODO: same as above for failures (mod attempting mod/admin ban)
 
-    })
+      console.log(chalk.yellow(`[${member.guild}]`) + `${member.user.username} is unkickable`);
+
+    });
 
 
   }
