7d5da8222ff5e8de4b8692de5fe11daade7e495f
Rokosz <legogattrooper@gmail.com>
2019-07-16 00:41:45 -0400

    filled with the proper shit

--- src/events/UAC.js
+++ src/events/UAC.js
@@ -2,6 +2,21 @@ const { RichEmbed } = require('discord.js');
 
 module.exports = async (oldUser, newUser) => {
 
+  if(oldUser.avatar === newUser.avatar) return;
 
+  const avatarChange = new RichEmbed();
+    .setTitle('Avatar Changed')
+    .setAuthor(`${newUser.tag}`)
+    .setDescription(`<@${newUser.id}> - *${newUser.id}*`)
+    .setColor('')
+    .addField()
+    .setFooter(`<@${newUser.id}> || *${newUser.id}*` + new Date())
+    .setThumbnail(oldUser.displayAvatarURL)
+
+  if(oldUser.avatar != newUser.avatar) {
+
+    
+
+  }
 
 }
