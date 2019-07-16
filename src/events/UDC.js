a53cc3b122570c5c83276cb968f3065a05a61e4a
Rokosz <legogattrooper@gmail.com>
2019-07-16 00:40:23 -0400

    filled with the proper parameters

--- src/events/UDC.js
+++ src/events/UDC.js
@@ -2,6 +2,21 @@ const { RichEmbed } = require('discord.js');
 
 module.exports = async (oldUser, newUser) => {
 
+  if(oldUser.discriminator === newUser.discriminator) return;
 
+  const discrimChange = new RichEmbed();
+    .setTitle('Discriminator Changed')
+    .setAuthor(`${newUser.tag}`)
+    .setDescription(`<@${newUser.id}> - *${newUser.id}*`)
+    .setColor('')
+    .addField('Old Discriminator', `**${oldUser.discriminator}**`)
+    .addField('New Discriminator', `**${newUser.discriminator}**`)
+    .setFooter(`<@${newUser.id}> || *${newUser.id}*` + new Date())
+
+  if(oldUser.discriminator != newUser.discriminator) {
+
+
+
+  }
 
 }
