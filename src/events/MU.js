372cca10679a88f9769c80d1c97b4a35899012ad
rokosz <46833498+rokosz@users.noreply.github.com>
2019-07-16 16:35:02 -0400

    added in some base functions will clean this up later.

--- src/events/MU.js
+++ src/events/MU.js
@@ -3,5 +3,23 @@ const { RichEmbed } = require('discord.js');
 module.exports = async (newMessage, oldMessage) => {
 
   if(newMessage.content === oldMessage.content) return;
-  
+  let logs = member.guild.channels.find(c => c.name === 'logs');
+
+  const messageUpdate = new RichEmbed();
+    .setTitle('Message Updated.')
+    .setAuthor(`${newMessage.author.tag}`)
+    .setDescription(`<@${newMessage.author.id}> - *${newMessage.author.id}*`)
+    .setColor('')
+    .addField('Old content', `**${oldMessage.content}**`, true)
+    .addField('New content', `**${newMessage.content}**`, true)
+    .setFooter(`<@${newMessage.author.id}> || *${newMessage.author.id}*` + new Date())
+
+  if(newMessage.content != oldMessage.content) {
+
+    logs.send(messageUpdate).then(console.log(chalk.yellow(`${newMessage.author.id} - ${newMessage.author.username} has changed their avatar.`)));
+
+    sql.prepare("INSERT INTO logs VALUES (?)").then(b => b.run([newMessage.author.id]));
+
+  }
+
 }
