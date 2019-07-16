f1a779f5763a9aa811ff0f388462d47bd399a109
Rokosz <legogattrooper@gmail.com>
2019-07-16 00:45:32 -0400

    added todo

--- src/events/UNC.js
+++ src/events/UNC.js
@@ -3,6 +3,23 @@ const { RichEmbed } = require('discord.js');
 module.exports = async (oldUser, newUser) => {
 
   if(oldUser.username === newUser.username) return;
-  
+
+  // TODO: setup database logging for username changes.
+    
+  const unameChange = new RichEmbed();
+    .setTitle('Username Changed')
+    .setAuthor(`${newUser.tag}`)
+    .setDescription(`<@${newUser.id}> - *${newUser.id}*`)
+    .setColor('')
+    .addField('Old Username', `**${oldUser.username}**`)
+    .addField('New Username', `**${newUser.username}**`)
+    .setFooter(`<@${newUser.id}> || *${newUser.id}*` + new Date())
+
+  if(oldUser.username != newUser.username) {
+
+
+
+  }
+
 
 }
