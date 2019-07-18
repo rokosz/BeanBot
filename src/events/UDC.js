048068527d0adc11e4b589e50968633ee4cb333b
Rokosz <legogattrooper@gmail.com>
2019-07-18 01:57:40 -0400

    updated logging method.

--- src/events/UDC.js
+++ src/events/UDC.js
@@ -3,23 +3,17 @@ const { RichEmbed } = require('discord.js');
 module.exports = async (oldUser, newUser) => {
 
   let logs = newUser.guild.channels.find(c => c.name === 'logs');
-  if(oldUser.discriminator === newUser.discriminator) return;
 
   // TODO: setup database logging for discriminator changes.
 
   const discrimChange = new RichEmbed()
     .setTitle('Discriminator Changed')
-    .setAuthor(`${newUser.tag}`)
-    .setDescription(`<@${newUser.id}> - *${newUser.id}*`)
+    .setDescription(`${newUser.id}`)
     .setColor('')
     .addField('Old Discriminator', `**${oldUser.discriminator}**`, true)
     .addField('New Discriminator', `**${newUser.discriminator}**`, true)
-    .setFooter(`<@${newUser.id}> || *${newUser.id}*` + new Date())
+    .setFooter(new Date())
 
-  if(oldUser.discriminator != newUser.discriminator) {
-
-    logs.send(avatarChange).then(console.log(chalk.yellow(`${newUser.id} - ${newUser.username} has changed their avatar.`)));
-    
-  }
+  logs.send(discrimChange);
 
 }
