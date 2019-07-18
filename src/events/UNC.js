c6170e71418ea363965629e78c56369b5e286274
Rokosz <legogattrooper@gmail.com>
2019-07-18 01:57:48 -0400

    updated logging method.

--- src/events/UNC.js
+++ src/events/UNC.js
@@ -1,26 +1,19 @@
 const { RichEmbed } = require('discord.js');
 
-module.exports = async (oldUser, newUser) => {
+module.exports = async (oldMember, newMember) => {
 
-  let logs = newUser.guild.channels.find(c => c.name === 'logs');
-  if(oldUser.username === newUser.username) return;
+  let logs = newMember.guild.channels.find(c => c.name === 'uwu-lounge');
 
   // TODO: setup database logging for username changes.
 
   const unameChange = new RichEmbed()
-    .setTitle('Username Changed')
-    .setAuthor(`${newUser.tag}`)
-    .setDescription(`<@${newUser.id}> - *${newUser.id}*`)
-    .setColor('')
-    .addField('Old Username', `**${oldUser.username}**`, true)
-    .addField('New Username', `**${newUser.username}**`, true)
-    .setFooter(`<@${newUser.id}> || *${newUser.id}*` + new Date())
+    .setTitle('Nickname Changed')
+    .setDescription(`${newMember.id}`)
+    .setColor('#3498db')
+    .addField('Old Username', `**${oldMember.nickname}**`, true)
+    .addField('New Username', `**${newMember.nickname}**`, true)
+    .setFooter(new Date())
 
-  if(oldUser.username != newUser.username) {
-
-    logs.send(avatarChange).then(console.log(chalk.yellow(`${newUser.id} - ${newUser.username} has changed their avatar.`)));
-
-  }
-
+  logs.send(unameChange);
 
 }
