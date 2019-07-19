725b584478dbcc1d0c098f71cdb6a196691583a3
Rokosz <legogattrooper@gmail.com>
2019-07-19 14:17:57 -0400

    todo: fix shit

--- src/util/eventLoader.js
+++ src/util/eventLoader.js
@@ -2,16 +2,20 @@ const bean = (event) => require(__dirname + `/../events/${event}`);
 
 module.exports = (client) => {
 
-  // TODO: set up logging funcitons
+  // TODO: clean all of the logging into a single file for logs.
 
   client.on('ready', () => bean('ready')(client));
   client.on('guildMemberAdd', bean('GMA'));
   client.on('guildMemberRemove', bean('GMR'));
   client.on('guildMemberUpdate', bean('UNC'));
-  client.on('guildMemberUpdate', bean('UAC'));
-  client.on('guildMemberUpdate', bean('UDC'));
+
+/*
+  client.on('guildMemberUpdate', bean('UAC')); @error { logging when user also updates nick }
+  client.on('guildMemberUpdate', bean('UDC')); @errpr { logging when user also updates nick/discrim/ava }
+*/
 
   // experimental
+
   client.on('messageUpdate', eventLoad('MU'));
   client.on('messageDelete', eventLoad('MD'));
 
@@ -19,4 +23,5 @@ module.exports = (client) => {
   client.on('channelCreate', eventLoad('CC'));
   client.on('channelDelete', eventLoad('CD'));
   */
+
 }
