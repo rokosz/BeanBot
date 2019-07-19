a729ba158b80e4b1cb4fcdf21219ea276a22b7c2
Rokosz <legogattrooper@gmail.com>
2019-07-18 22:59:12 -0400

    updated method

--- src/util/eventLoader.js
+++ src/util/eventLoader.js
@@ -7,9 +7,9 @@ module.exports = (client) => {
   client.on('ready', () => bean('ready')(client));
   client.on('guildMemberAdd', bean('GMA'));
   client.on('guildMemberRemove', bean('GMR'));
-  client.on('userUpdate', bean('UNC'));
-  client.on('userUpdate', bean('UAC'));
-  client.on('userUpdate', bean('UDC'));
+  client.on('guildMemberUpdate', bean('UNC'));
+  client.on('guildMemberUpdate', bean('UAC'));
+  client.on('guildMemberUpdate', bean('UDC'));
 
   // experimental
   client.on('messageUpdate', eventLoad('MU'));
