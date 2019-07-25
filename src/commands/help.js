c6abf09ec8ffccfc3a26de4bd3ff74af57c9767f
Rokosz <legogattrooper@gmail.com>
2019-07-25 14:26:56 -0400

    updated to match

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,23 +1,17 @@
 const { RichEmbed } = require('discord.js');
 const fs = require('fs');
 
-exports.run = async (client, message) => {
+module.exports.run = async (client, message) => {
 
   // just addressing each command. Could be done otherwise. (see previous commit).
 
   const embed = new RichEmbed()
     .setTitle('Command help')
-    .addField('Public commands', 'b.help, b.hex, b.serverinfo, b.userinfo, b.verify')
-    .addField('Moderation commands', 'b.ban, b.kick, b.warn, b.lockdown, b.mute')
+    .addField('Public commands', 'b.help, b.hex, b.serverinfo, b.userinfo, b.verify, b.level, b.leaderboard')
+    .addField('Moderation commands', 'b.ban, b.kick, b.warn, b.lockdown, b.mute, b.eval')
     .setFooter('BeanBot © rokosz 2019')
     .setTimestamp()
 
   message.channel.send(embed);
 
-}
-
-module.exports.help = {
-  name: 'help',
-  description: 'displays the commands',
-  useage: `b.`
 }
